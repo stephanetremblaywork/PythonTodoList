13669b3619d71ca02b7d60484cf80cc1491fd637
Stephane Tremblay <stephane.tremblay.2@gmail.com>
2022-05-13 17:12:03 -0400

    Made an app skeleton, WIP....

--- API/ToDoListAPI/app.py
+++ API/ToDoListAPI/app.py
@@ -1,10 +1,16 @@
-
+#!/usr/bin/python
 
 class App():
 
-    def __init__(self) -> None:
-        pass
+    def __init__(self, ip_address, port) -> None:
+        self._ip_address = ip_address
+        self._port = port
 
     def run():
         pass
-    
+
+def main():
+    print("Hello World")
+
+if __name__ == "__main__":
+    main()
