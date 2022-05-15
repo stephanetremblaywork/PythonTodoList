c7c4cf644736dbabbfffb23bc1e6a234faf24e2b
Stephane Tremblay <stephane.tremblay.2@gmail.com>
2022-05-14 20:00:43 -0400

    Moved files around to organize the project a little better. still have to change the imports accordingly.

---
+++ api/to_do_list/__init__.py
@@ -0,0 +1,4 @@
+from .to_do_item import ToDoItem
+from .to_do_list import ToDoList
+
+__all__ = ["ToDoItem", "ToDoList"]
---
+++ api/to_do_list/tests/to_do_item_test.py
@@ -0,0 +1,13 @@
+from unittest import *
+from to_do_item import ToDoItem
+
+class ToDoItemTestCase(TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def testA(self):
+        self.assertTrue(False, "Default fail test")
