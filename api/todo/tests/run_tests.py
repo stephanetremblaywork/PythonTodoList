fc949ad9d65840ef8ed4d0c8b3dca1fcdf8dd39c
Stephane Tremblay <steph.tremblay@cgi.com>
2022-05-18 19:48:01 -0400

    Added the unit test skeleton for the ToDoList class

--- api/todo/tests/run_tests.py
+++ api/todo/tests/run_tests.py
@@ -1,10 +1,12 @@
 from unittest import TestSuite
 from unittest import TextTestRunner
 from api.todo.tests.todo_item_test import ToDoItemTestCase
+from api.todo.tests.todo_list_test import ToDoListTestCase
 
 def getTodoTestSuite():
     suite = TestSuite()
     suite.addTest(ToDoItemTestCase('Basic ToDoItem class unit tests.'))
+    suite.addTest(ToDoListTestCase('Basic ToDoList class unit tests.'))
     return suite
 
 if __name__ == '__main__':
---
+++ api/todo/tests/todo_list_test.py
@@ -0,0 +1,35 @@
+from unittest import TestCase
+
+from api.todo.todo_list import ToDoList
+
+class ToDoListTestCase(TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_init(self) -> None:
+        self.fail("Fail by default.")
+
+    def test_getAllItems(self) -> None:
+        self.fail("Fail by default.")
+
+    def test_getNotDoneItems(self) -> None:
+        self.fail("Fail by default.")
+
+    def test_getDoneItems(self) -> None:
+        self.fail("Fail by default.")
+
+    def test_getItemAtPosition(self) -> None:
+        self.fail("Fail by default.")
+
+    def test_addToDoItem(self) -> None:
+        self.fail("Fail by default.")
+
+    def test_addToDoItem(self) -> None:
+        self.fail("Fail by default.")
+
+    def test_moveItem(self) -> None:
+        self.fail("Fail by default.")
